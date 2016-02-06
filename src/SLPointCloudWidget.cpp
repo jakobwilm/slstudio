4510cb018006a546f95d7f86430e7c0cb5697561
Nan Zhang <zhangnan@baysenvision.com>
2016-02-06 12:10:31 +0800

    cv Mat has to be same dimension as the Eigen::Affine3f

--- src/SLPointCloudWidget.cpp
+++ src/SLPointCloudWidget.cpp
@@ -57,15 +57,16 @@ void SLPointCloudWidget::updateCalibration(){
 
     CalibrationData calibration;
     bool load_result = calibration.load("calibration.xml");
-	if (!load_result)
-		return;
+// 	if (!load_result)
+// 		return;
     // Camera coordinate system
     visualizer->addCoordinateSystem(50, "camera", 0);
 
     // Projector coordinate system
-    cv::Mat TransformPCV(3, 4, CV_32F);
-    cv::Mat(calibration.Rp).copyTo(TransformPCV.colRange(0, 3));
-    cv::Mat(calibration.Tp).copyTo(TransformPCV.col(3));
+    cv::Mat TransformPCV(4, 4, CV_32F, 0.0);
+	cv::Mat(calibration.Rp).copyTo(TransformPCV.colRange(0, 3).rowRange(0, 3));
+	cv::Mat(calibration.Tp).copyTo(TransformPCV.col(3).rowRange(0, 3));
+	TransformPCV.at<float>(3, 3) = 1.0;
     Eigen::Affine3f TransformP;
     cv::cv2eigen(TransformPCV, TransformP.matrix());
 
